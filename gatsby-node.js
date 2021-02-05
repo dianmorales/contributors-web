cbcb9484dc71c278cc8ac5e1046365c8838639e6
Diana Morales <dianmorales@gmail.com>
2021-02-05 14:53:52 +0100

    exclude bots

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 const token = process.env.VERDACCIO_TOKEN;
 const contributors = require('@dianmora/contributors');
 const excludebots = ['verdacciobot', 'dependabot-preview[bot]', 'dependabot[bot]', '64b2b6d12bfe4baae7dad3d01', 'greenkeeper[bot]', 'snyk-bot', 
-                    'allcontributors[bot]', 'renovate[bot]', 'undefined'];
+                    'allcontributors[bot]', 'renovate[bot]', 'undefined', 'renovate-bot'];
 exports.sourceNodes = async ({
   actions: { createNode },
   createContentDigest,
